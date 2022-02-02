5063ca0588e162ca874c773828dc6954879ea3fd
Dan Kotlyar <dan.kotlyar@me.gatech.edu>
2022-02-02 11:29:46 -0500

    [DEV] Basic containers
    Almost finalized the structure of the basic containers.

--- xsInterface/containers/branchesdata.py
+++ xsInterface/containers/branchesdata.py
@@ -9,62 +9,7 @@ Last updated on Tue Feb 01 13:30:00 2022 @author: Dan Kotlyar
 email: dan.kotlyar@me.gatech.edu
 """
 
-
-class SingleSet:
-    """Container that stores the most basic data set
-
-    Parameters
-    ----------
-    dataLabel : dict
-        Labels that describe the type of data inputted to the set.
-        Keys describe the name of the stored attributed and values the
-        corresponding description used for error tracking.
-    xsLabel : dict
-        specific label description only for xs.
-    ngroups : int
-        Number of energy groups. 
-
-
-    Attributes
-    ----------
-    COMPLETE
-
-    """
-
-    def __init__(self, dataLabel, xsLabel, ngroups):
-        """Assign parameters that describe the flow"""
-
-        self._dataLabel = dataLabel  # could also be defined in XsSets
-        self._xsLabel = xsLabel
-        self._ngroups = ngroups
-        
-    def dataIn(self, dataType, **kwargs):
-        """Feed in cross sections"""
-        # dataType will allow us to control whether this a matrix, float,
-        # vector
-        # **kwargs:
-        # name of the cross sections and their correponsing values
-        # 
-        # we can write this method in one go or multiple times depending on
-        # the material being fed to the container.
-        pass
-    
-    def condense(self, listAttributes, eneCutoffs):
-        """Energy condensation"""
-        # Condensation will be performed only for the attributes of interest
-        # energy cutoffs or indices must be provided
-        # The method will return a NEW object, which could be stored indep.
-        # The condensed results can also be stored on a muted dictionary
-        # in the container
-        pass
-    
-    def getvalues(self, *attributes):
-        """obtain the data of specific attributes"""
-        # can be return as a separate results container.
-        pass
-
-
-class BranchoffSets:
+class BranchesSets:
     """A container to store sets for different operational conditions
 
     Methods
@@ -75,7 +20,7 @@ class BranchoffSets:
 
     """
 
-    def __init__(self, ndep, states, depInterp):
+    def __init__(self, ndep, states, legend, depInterp):
         # Init to empty dictionary
         self._datasets = {}
         self._ndep = ndep  # integer to the number of total dependencies
@@ -101,7 +46,10 @@ class BranchoffSets:
         pass
 
 
-    def getset(self, state):
+    def intrpset(self, layer=time, interpFlag):
+        
+
+    def getset(self, state=(time = 4), interpFlag):
         """Obtains the NodeResults object for a specific node
 
         Parameters
@@ -130,7 +78,7 @@ class BranchoffSets:
     def __getitem__(self, pos):
         return self._datasets[pos]
 
-    def getvalues(self, attributes, interpFlag):
+    def getvalues(self, attributes, state, interpFlag=True):
         """Obtain values of a specific attribute over different states"""
 
         # return vals
