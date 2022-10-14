e69d037e99cc24413aabc0e1f8d8fce9b651325d
Dan Kotlyar <dan.kotlyar@me.gatech.edu>
2022-10-14 18:54:00 -0400

    [BUG] Read Shift Input
    A bug when reading multiple universes from the same shift results files was fixed.
    The bug originated due the lack of defining the ``attrs = rc.macro + rc.micro + rc.meta + rc.kinetics``

---
+++ xsInterface/inputsets/inp5/exe_xs_shift.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""exe_xs_shift.py
+
+ SHIFT outputs
+
+
+Created on Wed Sep 14 06:00:00 2022 @author: Dan Kotlyar
+Last updated on Wed Sep 14 06:00:00 2022 @author: Dan Kotlyar
+
+email: dan.kotlyar@me.gatech.edu
+
+"""
+from xsInterface.functions.main import Main
+from omnWrite import omnGen
+
+inputFile = ".\\controlDict"
+
+# Read the control dict
+xs = Main(inputFile)
+
+# Read xs data and templates and populate data
+xs.Read()
+
+# Write data to txt files
+xs.Write()
+
+
+#      NG  X        Y        Z  Part/cy  cy   ina np  pO nx  ny  nz
+# omnGen(2,  18.8976, 18.8976, 1, 100000,  150, 25, 24, 5, 3,  3,  1)
